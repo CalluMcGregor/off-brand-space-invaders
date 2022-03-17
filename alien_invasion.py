cfa64f5c8f43c3e09b83fa593137aa67cf066e46
Callum McGregor <101708269+Estoni4@users.noreply.github.com>
2022-03-17 10:04:02 +0000

    Add files via upload

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,6 +27,13 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self.bullets.update()
+
+            #delete old bullet
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+            print(len(self.bullets))
+            
             self._update_screen()
 
     def _check_events(self):
